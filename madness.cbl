c3fac4360c9a1624dd05e5316f96ed3dd145ce40
HotFazz <basalts.darter-0a@icloud.com>
2022-11-18 14:05:13 +0100

    more commits

--- madness.cbl
+++ madness.cbl
@@ -1,28 +1,22 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. madness.
-       AUTHOR. Avid Fayaz
-
+       PROGRAM-ID. MADNESS.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       *> factorial in COBOL
 
-         77 factorial pic 9(15) comp.
-         77 n pic 99.
-         77 i pic 99.
-         77 ist pic XX.
-         77 factorialSet pic X(18).
 
+       *> Factorial in COBOL
+       77 N PIC 9(4).
+       77 A PIC S9(4) VALUE 0.
+       77 F PIC 9(4) VALUE 1.       
        PROCEDURE DIVISION.
-         move 16 to n
-         move 0 to i
-         move 1 to factorial
-         perform until i greater than n
-           move i to ist
-           move factorial to factorialSet
-           display ist "! = " factorialSet
-           add 1 to i
-           multiply i by factorial
-             on size error display "input exceeds limit!"
-           end-multiply
-         end-perform.
-         stop run.
+       PARA.
+           DISPLAY "Enter a number: ".
+           ACCEPT N.
+
+           PERFORM PARA1 UNTIL A = N.
+           DISPLAY "Result: ".
+           DISPLAY F.
+           STOP RUN.
+       PARA1.
+           ADD 1 TO A.
+           COMPUTE F = F * A.
